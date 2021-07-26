0b187ad8660cf86c22164a69f9512e07267cf0c2
younes-barhouni <y.barhouni@gmail.com>
2021-07-26 08:17:22 +0200

    update port number

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ const config = {
     open: true,
     host: "localhost",
     contentBase: path.join(__dirname, "dist"),
-    port: 3002,
+    port: 3000,
   },
   plugins: [
     new HtmlWebpackPlugin({
